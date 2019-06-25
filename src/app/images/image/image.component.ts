45302f6ca3fed60bfbc9af0f2a9204683681074b
SharkyUgas <sharmarke.ugas@outlook.com>
2019-06-25 11:32:55 +0100

    added image preview

--- src/app/images/image/image.component.ts
+++ src/app/images/image/image.component.ts
@@ -8,6 +8,9 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ImageComponent implements OnInit {
 
+  imgSrc : string = '/assets/img/image_placeholder.jpg';
+  selectedImage: any = null;
+
   formTemplate = new FormGroup({
     caption : new FormControl(''),
     category : new FormControl(''),
@@ -19,6 +22,19 @@ export class ImageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showPreview(event:any){
+    if(event.target.files && event.target.files[0]){
+      const reader = new FileReader();
+      reader.onload = (e:any) => this.imgSrc = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files[0];
+    }
+  else{
+    this.imgSrc='/assets/img/image_placeholder.jpg';
+    this.selectedImage = null;
+  }  
   }
 
 }
